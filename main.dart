313664cb6600e47d56f34bb73860d6090571de3d
Bobby Seidensticker <bseidensticker@gmail.com>
2025-08-16 21:28:01 -0700

    Blindly committing a bunch of crap in main.dart, I think it's from me trying to get dithering working properly but I know it didn't work

--- main.dart
+++ main.dart
@@ -42,14 +42,25 @@ Future<Image> imageFromFilename(String filename, int widthInPixels) async {
   return (await cmd.getImage())!;
 }
 
+List<List<int>> myImageFromImage(Image image, int w, int h) {
+  List<List<int>> result = <List<int>>[];
+  for (int y = 0; y < h; y++) {
+    result.add(<int>[]);
+    for (int x = 0; x < w; x++) {
+      result[y].add(image.getPixel(x,y).r.floor());
+    }
+  }
+  return result;
+}
+
 Future<Image> processImage(
     String filename, int widthInPixels, int numberOfColors) async {
   final image = await imageFromFilename(filename, widthInPixels);
   await File('${filename}_${widthInPixels}.png').writeAsBytes(encodePng(image));
 
   final ditheredImage = ditherImage(image,
-      quantizer: NeuralQuantizer(image,
-          numberOfColors: numberOfColors, samplingFactor: 1),
+    quantizer: NeuralQuantizer(image,
+              numberOfColors: numberOfColors, samplingFactor: 10),
       kernel: DitherKernel.atkinson);
 
   await File('${filename}_${widthInPixels}_dithered_${numberOfColors}.png')
@@ -58,9 +69,11 @@ Future<Image> processImage(
   return ditheredImage;
 }
 
+
 String format(double n) {
   return n.toStringAsFixed(n.truncateToDouble() == n ? 0 : 2);
 }
+
 
 // Configuration class to hold all the constant variables
 class ImageConfig {
@@ -77,18 +90,27 @@ class ImageConfig {
   final double wallWidth;
   final double bottomThk;
   final double layerHeight;
+  final bool shouldDoPlusWalls;
+  final int numberOfColorsOverride;
   // The maximum height of a wall. If a pixel is black, the wall will be this many mm over the base.
   // Max height is as tall as the non-wall width of the cell, ideal light source would be at a 45 degree angle.
-  double get maxHeight => cellSize - wallWidth;
+  double get maxHeight => numberOfColors * layerHeight;
+
   double get border => cellSize;
+
 
   // Calculated number of colors based on maxHeight and layerHeight
   //
   // Subtract one because every cell must have a wall to avoid blank spots.
-  int get numberOfColors => (maxHeight / layerHeight).floor() - 1;
+  int get numberOfColors => numberOfColorsOverride == 0 ? (((cellSize - wallWidth) / layerHeight).floor()) : numberOfColorsOverride;
 
   String get outputFilename =>
-      '${widthInPixels}px_${format(cellSize)}cell_${format(wallWidth)}wall_${format(maxHeight)}maxHeight_${horizImageFilename}_${vertImageFilename}.scad';
+      '${widthInPixels}px_${format(cellSize)}cell_${format(wallWidth)}wall_${format(maxHeight)}maxHeight_${horizImageFilename}_${vertImageFilename}_pluswalls_${shouldDoPlusWalls}.scad';
+
+  @override
+  String toString() {
+    return 'ImageConfig. cellSize-wallWidth: ${cellSize - wallWidth} layerHeight: $layerHeight, ${(maxHeight / layerHeight).floor() - 1} maxHeight, numberOfColors: $numberOfColors layerHeight: $layerHeight maxHeight: ${numberOfColors * layerHeight}';
+  }
 
   // Constructor to initialize all the variables
   const ImageConfig({
@@ -101,6 +123,8 @@ class ImageConfig {
     this.wallWidth = 0.4,
     this.bottomThk = 0.8,
     this.layerHeight = 0.1,
+    this.shouldDoPlusWalls = false,
+    this.numberOfColorsOverride = 0,
   });
 }
 
@@ -122,21 +146,37 @@ void main() async {
   const fuji = ImageConfig(
       horizImageFilename: 'great_wave.jpg',
       vertImageFilename: 'red_fuji.jpg',
-      widthInPixels: 150,
+      widthInPixels: 100,
       cellSize: 1.1,
       wallWidth: 0.22,
       bottomThk: 0.6,
+      layerHeight: 0.05,
+      shouldDoPlusWalls: false);
+  const p4Config = ImageConfig(
+      horizImageFilename: 'family_edited.jpg',
+      vertImageFilename: 'babies_edited.jpg',
+      widthInPixels: 200,
+      cellSize: 1.1,
+      wallWidth: 0.22,
+      bottomThk: 1.0,
       layerHeight: 0.05);
 
-  await generate(fuji);
+
+  print('fuji: $fuji');
+
+  await generate(p4Config);
 }
+
+double roundToLayerHeight(double height, double layerHeight) =>
+(height / layerHeight).round() * layerHeight;
+
 
 Future<void> generate(ImageConfig config) async {
   final horizImage = await processImage(
       config.horizImageFilename, config.widthInPixels, config.numberOfColors);
 
   final vertImage = await processImage(
-      config.vertImageFilename, config.widthInPixels, config.numberOfColors);
+    config.vertImageFilename, config.widthInPixels, config.numberOfColors);
 
   print('writing to ${config.outputFilename}');
   final outFile = File(config.outputFilename);
@@ -163,37 +203,77 @@ Future<void> generate(ImageConfig config) async {
     vertEndY = horizImage.height;
   }
 
+  final myHorizImage = myImageFromImage(horizImage, config.widthInPixels, horizEndY);
+  final myVertImage = myImageFromImage(vertImage, config.widthInPixels, vertEndY);
+
+  print('horiz size: ${horizImage.width}, ${horizImage.height}');
+  print('my horiz size: ${myHorizImage[0].length}, ${myHorizImage.length}');
+  print('true horiz size: ${config.widthInPixels}, ${horizEndY}');
+  print('vert size: ${vertImage.width}, ${vertImage.height}');
+  print('my vert size: ${myVertImage[0].length}, ${myVertImage.length}');
+  print('true vert size: ${config.widthInPixels}, ${vertEndY}');
+  
+
+
+
+
   if (config.doHorizImage) {
+    final debugHeights = <double, int>{};
     for (int y = 0; y < horizEndY; y++) {
       for (int x = 0; x < horizImage.width; x++) {
         // Get the value from 0 (black) to 1 (white) of the red channel.  The image is greyscale so
         // the channels should all be equal to each other.
-        final val = horizImage.getPixel(x, y).r;
+        final val = myHorizImage[y][x];
         final wallHeight =
-            (1 - val / 256) * (config.maxHeight - config.layerHeight);
+        roundToLayerHeight((1 - val / 256) * (config.maxHeight - config.layerHeight), config.layerHeight);
+        debugHeights[wallHeight] = debugHeights[wallHeight] == null
+            ? 0
+            : debugHeights[wallHeight]! + 1;
 
         leftWalls.add('''
           back(${config.border + (horizEndY - y) * config.cellSize})
-          right(${config.border + (x + 1) * config.cellSize})
+          right(${config.border + (x + 1) * config.cellSize + (config.shouldDoPlusWalls ? 0.5 * (config.cellSize-config.wallWidth) : 0)})
           up(0.1)
           cuboid([${config.wallWidth + 0.01}, ${config.cellSize + 0.01}, ${config.bottomThk - 0.1 + wallHeight + config.layerHeight}], align=V_RIGHT+V_BACK+V_UP);''');
       }
     }
+
+    var sortedKeys = debugHeights.keys.toList()..sort();
+
+    for (var height in sortedKeys) {
+      print('DEBUG heights, ${debugHeights[height]} instances of $height');
+    }
+
+    print(
+        'DEBUG unique heights: ${Set<double>.from(debugHeights.keys).length}');
   }
+
   if (config.doVertImage) {
+    final debugHeights = <double, int>{};
     for (int y = 0; y < vertEndY; y++) {
       for (int x = 0; x < vertImage.width; x++) {
-        final val = vertImage.getPixel(x, y).r;
+        final val = myVertImage[y][x];
         final wallHeight =
-            (1 - val / 256) * (config.maxHeight - config.layerHeight);
+        roundToLayerHeight((1 - val / 256) * (config.maxHeight - config.layerHeight), config.layerHeight);
+            debugHeights[wallHeight] = debugHeights[wallHeight] == null
+            ? 0
+            : debugHeights[wallHeight]! + 1;
 
         upWalls.add('''
-          back(${config.border + (vertEndY - y) * config.cellSize})
+          back(${config.border + (vertEndY - y) * config.cellSize + (config.shouldDoPlusWalls ? 0.5 * (config.cellSize-config.wallWidth) : 0)})
           right(${config.border + (x + 1) * config.cellSize})
           up(0.1)
           cuboid([${config.cellSize + 0.01}, ${config.wallWidth + 0.01}, ${config.bottomThk - 0.1 + wallHeight + config.layerHeight}], align=V_RIGHT+V_BACK+V_UP);''');
       }
     }
+    var sortedKeys = debugHeights.keys.toList()..sort();
+
+    for (var height in sortedKeys) {
+      print('DEBUG heights, ${debugHeights[height]} instances of $height');
+    }
+
+    print(
+        'DEBUG unique heights: ${Set<double>.from(debugHeights.keys).length}');
   }
 
   outSink.write('''
